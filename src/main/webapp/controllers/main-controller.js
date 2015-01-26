48315c4745eef56fb956c332921388686118d260
Prerna90 <prerna.singhal90@gmail.com>
2015-01-26 22:38:20 +0530

    Making backend URL call as HTTPS

--- src/main/webapp/controllers/main-controller.js
+++ src/main/webapp/controllers/main-controller.js
@@ -4,13 +4,16 @@ app.controller('MainController', ['$scope', '$http', '$location', '$rootScope',
 	$scope.test = "Yay";
 	
 	$http({
-		method : 'GET',
-		url : "https://deals.expedia.com/beta/deals/hotels.json",
+		method : 'POST',
+		url : "http://deals.expedia.com/beta/deals/hotels.json",
 		headers : {
 			'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
 		},
 	}).success(function(data) {
 		console.log("Success !!");
+		console.log(data);
+	}).error(function(data) {
+		console.log("Errorrr");
 		console.log(data);
 	});
 	
